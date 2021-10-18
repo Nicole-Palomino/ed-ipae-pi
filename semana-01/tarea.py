e42f1a6450931462ac8f820d106ae8fc29369b7e
Nicole <alessa.kz.fx@gmail.com>
2021-10-17 20:35:43 -0500

    cambios agregando la funcion

--- semana-01/tarea.py
+++ semana-01/tarea.py
@@ -1,12 +1,17 @@
 numeros=[]
+
+def ordenar():
+    ordenado = sorted(numeros)
+    print('La lista ordenada: ',ordenado)
+
 x=int(input('Cuántos números desea ingresar? '))
+
 if (x >= 10):
     print('**Ingrese los',x, 'números**')
     for n in range(x):
         num=int(input('Ingrese un número: '))
         numeros.append(num)
     print(numeros)
-    ordenados = sorted(numeros)
-    print('La lista ordenada: ',ordenados)
+    ordenar()
 else:
-    print('Mínimo se deben ingresar 10 números')    
+    print('Mínimo se deben ingresar 10 números') 
